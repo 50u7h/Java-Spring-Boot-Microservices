e8b4281783a1df9a6ad41769d9e7553a856d2c40
50u7h <50u7h@protonmail.com>
2023-04-06 11:25:17 +0300

    Creating User Entity and some test data

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user_details(id,birth_date,name) values (10001, current_date(), 'test TEST')
+insert into user_details(id,birth_date,name) values (10002, current_date(), 'qwerty QWERTY')
+insert into user_details(id,birth_date,name) values (10003, current_date(), 'asdfgh ASDFGH')
