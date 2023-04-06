b2430b4d0624ae3be927991f88597259a8e31f63
50u7h <50u7h@protonmail.com>
2023-04-06 15:53:32 +0300

    Creating a hard coded limits service

---
+++ microservices/limits-service/src/main/java/com/guney/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,14 @@
+package com.guney.microservices.limitsservice;
+
+import com.guney.microservices.limitsservice.bean.LimitConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsConfigurationController {
+
+    @GetMapping("/limits")
+    public LimitConfiguration retrieveLimitsFromConfigurations() {
+        return new LimitConfiguration(1000, 1);
+    }
+}
