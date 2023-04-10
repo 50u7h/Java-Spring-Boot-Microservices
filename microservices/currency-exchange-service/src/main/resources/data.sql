c3a7882021754770e00045bc276945a476d99bbb
50u7h <50u7h@protonmail.com>
2023-04-10 12:59:04 +0300

    Configure JPA and Initialized Data - V2

---
+++ microservices/currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+values (10001, 'USD', 'TRY', 19, '');
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+values (10002, 'EUR', 'TRY', 21, '');
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+values (10003, 'GBP', 'TRY', 23, '');
